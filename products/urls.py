30b189cb7521aa4235b3657d539088b6442942c9
김민규 <skystar_pisces@gimmingyuui-MacBookAir.local>
2021-07-01 11:46:02 +0900

    Add : review 기능 - review tests.py 작성 - csv.zip 삭제

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls    import path
 
-from products.views import ExpertView, ProductDetailView, ProductView
+from products.views import ExpertView, ProductDetailView, ProductReviewView, ProductView
 
 urlpatterns = [
     path('', ProductView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('/expert', ExpertView.as_view())
+    path('/expert', ExpertView.as_view()),
+    path('/<int:product_id>/review', ProductReviewView.as_view())
 ]
